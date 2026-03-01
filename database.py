fee433d091ccbff5455e32ebb61725a0cac6ef31
Priyanshi Vyas <priyanshivyas2006@gmail.com>
2026-03-01 19:26:53 +0530

    fully migrate to postgres

--- database.py
+++ database.py
@@ -1,38 +1,43 @@
-import pyodbc
-from config import Config
+import os
+import psycopg2
 import bcrypt
+
 
 def get_db_connection():
     try:
-        conn = pyodbc.connect(
-            "DRIVER={ODBC Driver 17 for SQL Server};"
-            "SERVER=localhost;"
-            "DATABASE=attendify;"
-            "Trusted_Connection=yes;"
+        DATABASE_URL = os.environ.get("DATABASE_URL")
+
+        conn = psycopg2.connect(
+            DATABASE_URL,
+            sslmode="require"
         )
-        print("DB CONNECTED SUCCESSFULLY")
+
+        print("POSTGRES CONNECTED")
         return conn
+
     except Exception as e:
-        print("REAL ERROR:", e)
-        return None    
-    
-    
+        print("DB ERROR:", e)
+        return None
+
+
 def hash_password(password):
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
 
+
 def check_password(password, hashed):
     return bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
+
 
 def fetchone_dict(cursor):
     row = cursor.fetchone()
     if row is None:
         return None
-    columns = [column[0] for column in cursor.description]
+    columns = [desc[0] for desc in cursor.description]
     return dict(zip(columns, row))
+
 
 def fetchall_dict(cursor):
     rows = cursor.fetchall()
-    columns = [column[0] for column in cursor.description]
+    columns = [desc[0] for desc in cursor.description]
     return [dict(zip(columns, row)) for row in rows]
-
